ca3e5e793aade6995f866b68cb02ca471e1976e1
einahage <einahage@cisco.com>
2015-04-09 16:00:15 +0200

    Moved to xcode6.3 branch of swiftyJSON

---
+++ OSX/Huebar/Huebar/Huebar-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Huebar-Bridging-Header.h
+//  Huebar
+//
+//  Created by Einar Hagen on 07/04/15.
+//  Copyright (c) 2015 Einar Hagen. All rights reserved.
+//
+
+#ifndef Huebar_Huebar_Bridging_Header_h
+#define Huebar_Huebar_Bridging_Header_h
+
+
+#endif
